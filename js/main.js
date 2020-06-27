aae0676e6dd0284d70ad1dc7a0c7b281567131bb
Vladislav Shivtsov <vdshivtsov@gmail.com>
2020-06-27 22:53:03 +0500

    Отрендерил карточку с тестовыми данными

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ var PHOTOS_ARRAY = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
-var TYPES_OF_FLAT_ARRAY = [
-  'palace',
-  'flat',
-  'house',
-  'bungalo'
-];
-
 var CHECKIN_TYPES_ARRAY = [
   '12:00',
   '13:00',
@@ -66,6 +59,22 @@ var DESCRIPTIONS_ARRAY = [
   'Эти апартаменты расположены в городе Дестин, всего в нескольких шагах от пляжа Дестин, пляжа Оушен-Вью и 8. К услугам гостей апартаментов Flr открытый бассейн, фитнес-центр, бар и бесплатный Wi-Fi.',
   'Апартаменты-студио Bay Front расположены на 4 этаже здания Bay Front, в городе Дестин, штат Сэнддестин. К услугам гостей бесплатный Wi-Fi, кондиционер, ресторан и сад с открытым бассейном.'
 ];
+
+var typesOfFlatDictionary = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'
+};
+
+var mapFromFeatureToClass = {
+  'wifi': 'popup__feature--wifi',
+  'dishwasher': 'popup__feature--dishwasher',
+  'parking': 'popup__feature--parking',
+  'washer': 'popup__feature--washer',
+  'elevator': 'popup__feature--elevator',
+  'conditioner': 'popup__feature--conditioner'
+};
 
 var mapPinsBlock = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -137,7 +146,8 @@ var getMocksArray = function () {
         'title': titles.splice(getRandomIntInclusive(0, titles.length - 1), 1)[0],
         'address': locationX + ', ' + locationY,
         'price': getRandomIntInclusive(10000, 500000),
-        'type': TYPES_OF_FLAT_ARRAY[getRandomIntInclusive(0, TYPES_OF_FLAT_ARRAY.length - 1)],
+        // 'type': TYPES_OF_FLAT_ARRAY[getRandomIntInclusive(0, TYPES_OF_FLAT_ARRAY.length - 1)],
+        'type': Object.keys(typesOfFlatDictionary)[getRandomIntInclusive(0, Object.keys(typesOfFlatDictionary).length - 1)],
         'rooms': getRandomIntInclusive(1, 10),
         'guests': getRandomIntInclusive(2, 30),
         'checkin': CHECKIN_TYPES_ARRAY[getRandomIntInclusive(0, CHECKIN_TYPES_ARRAY.length - 1)],
@@ -157,6 +167,52 @@ var getMocksArray = function () {
   return mocksArray;
 };
 
+var getCard = function (datum) {
+  var cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+  var announcementCard = cardTemplate.cloneNode(true);
+
+  announcementCard.querySelector('.popup__title').textContent = datum.offer.title;
+  announcementCard.querySelector('.popup__text--address').textContent = datum.offer.address;
+  announcementCard.querySelector('.popup__text--price').textContent = datum.offer.price + '₽/ночь';
+  announcementCard.querySelector('.popup__type').textContent = typesOfFlatDictionary[datum.offer.type];
+  announcementCard.querySelector('.popup__text--capacity').textContent = datum.offer.rooms + ' комнаты для ' + datum.offer.guests + ' гостей';
+  announcementCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + datum.offer.checkin + ', выезд до ' + datum.offer.checkout;
+  announcementCard.querySelector('.popup__description').textContent = datum.offer.description;
+  announcementCard.querySelector('.popup__avatar').src = datum.author.avatar;
+
+  var popupFeatures = announcementCard.querySelectorAll('.popup__feature');
+  for (var i = 0; i < popupFeatures.length; i++) {
+    var hasElement = false;
+    for (var j = 0; j < datum.offer.features.length; j++) {
+      if (popupFeatures[i].classList.contains(mapFromFeatureToClass[datum.offer.features[j]])) {
+        hasElement = true;
+        break;
+      }
+    }
+    if (!hasElement) {
+      popupFeatures[i].remove();
+    }
+  }
+
+  var popupPhotos = announcementCard.querySelector('.popup__photos').querySelectorAll('.popup__photo');
+  var popupPhotosFragment = document.createDocumentFragment();
+  for (i = 0; i < datum.offer.photos.length; i++) {
+    var photo = popupPhotos[0].cloneNode(true);
+    photo.src = datum.offer.photos[i];
+    popupPhotosFragment.append(photo);
+  }
+  announcementCard.querySelector('.popup__photos').appendChild(popupPhotosFragment);
+  popupPhotos[0].remove();
+
+  return announcementCard;
+};
+
+var renderCard = function (testDatum) {
+  document.querySelector('.map__filters-container').insertAdjacentElement('beforebegin', testDatum);
+};
+
 var testData = getMocksArray();
 switchMapMode();
 renderMapPins(testData);
+renderCard(getCard(testData[0]));
+
